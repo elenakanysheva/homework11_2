c04d5be4ba66e176f4c77420dc7defd385889a2d
Elena Kanysheva <elenakanicheva@gmail.com>
2023-06-28 00:13:51 +0500

    3 functions

--- str_func.py
+++ str_func.py
@@ -1,9 +1,15 @@
 def up_str(x):
-    """Все буквы заглавные"""
+    """Все буквы
+    заглавные"""
     return x.upper()
 
 
 def title_str(x):
     """Первая буква заглавная"""
     return x.title()
+
+
+def low_str(x):
+    """Все буквы строчные"""
+    return x.lower()
 
